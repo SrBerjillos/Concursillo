c3742c8e6d0345444bfd364b485b0b400a0e1e33
Álvaro <alesiito69@gmail.com>
2025-03-27 17:00:14 +0100

    Primer commit

--- server.js
+++ server.js
@@ -28,7 +28,6 @@ io.on('connection', (socket) => {
 });
 
 // Configurar el puerto en el que el servidor estará escuchando
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+server.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
 });
